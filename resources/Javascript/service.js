519375d5e8fa8cbe1e148bed3f02d8a1cc955136
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-04-13 18:05:00 -0300

    comenzando el algoritmo

---
+++ resources/Javascript/service.js
@@ -0,0 +1,23 @@
+const cambiar = {
+    e: "enter",
+    i: "imes",
+    a: "ai",
+    o: "ober",
+    u: "ufat",
+}
+const Encriptar = () => {
+    let newString = "";
+    let mensaje = texto.value;
+    
+
+}
+const Desencriptar = () => {
+    return
+
+}
+
+let texto = document.querySelector("textarea");
+let actionEn = document.getElementById("encriptarButton")
+let actionDes = document.getElementById("desencriptarButton")
+actionEn.onclick = Encriptar;
+actionDes.onclick = Desencriptar;
